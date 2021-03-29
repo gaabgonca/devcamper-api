0c225aa652e047629def8180d013e0cb6e88eaf0
[Gabriel GC] <[gaabgonca@gmail.com]>
2021-03-29 10:38:55 -0500

    Added advanced results for Bootcamps

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,9 @@ const {
   bootcampPhotoUpload,
 } = require('../controllers/bootcamps')
 
+const Bootcamp = require('../models/Bootcamp')
+const advancedResults = require('../middleware/advancedResults')
+
 // Include other resources
 const courseRouter = require('./courses')
 
@@ -17,7 +20,10 @@ const router = express.Router()
 //Reroute into other resource routers
 router.use('/:bootcampId/courses', courseRouter)
 
-router.route('/').get(getBootcamps).post(createBootcamp)
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp)
 
 router.route('/:id/photo').put(bootcampPhotoUpload)
 
