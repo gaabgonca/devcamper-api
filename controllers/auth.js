437a54932b5e5111b87ce199948e01447e2c302e
[Gabriel GC] <[gaabgonca@gmail.com]>
2021-04-11 07:30:27 -0500

    Get logged user via token

--- controllers/auth.js
+++ controllers/auth.js
@@ -73,6 +73,14 @@ const sendTokenResponse = (user, statusCode, res) => {
   })
 }
 
-// @desc Login user
-// @route POST /api/v1/auth/login
-// @access public
+// @desc Get current logged in user
+// @route POST /api/v1/auth/me
+// @access Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id)
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  })
+})
