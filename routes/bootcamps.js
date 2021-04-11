583eef5fab1e8a490167bab653ab15f8c4ae0116
[Gabriel GC] <[gaabgonca@gmail.com]>
2021-04-11 08:28:45 -0500

    Added authorization for routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -17,7 +17,7 @@ const courseRouter = require('./courses')
 
 const router = express.Router()
 
-const { protect } = require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
 //Reroute into other resource routers
 router.use('/:bootcampId/courses', courseRouter)
@@ -25,15 +25,17 @@ router.use('/:bootcampId/courses', courseRouter)
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(protect, createBootcamp)
+  .post(protect, authorize('publisher', 'admin'), createBootcamp)
 
-router.route('/:id/photo').put(protect, bootcampPhotoUpload)
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload)
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp)
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp)
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 
--- routes/courses.js
+++ routes/courses.js
@@ -17,7 +17,7 @@ const advancedResults = require('../middleware/advancedResults')
 
 const router = express.Router({ mergeParams: true })
 
-const { protect } = require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
 router
   .route('/')
@@ -28,13 +28,13 @@ router
     }),
     getCourses
   )
-  .post(protect, createCourse)
+  .post(protect, authorize('publisher', 'admin'), createCourse)
 
 router
   .route('/:id')
   .get(getCourse)
-  .put(protect, updateCourse)
-  .delete(protect, deleteCourse)
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse)
 // .post(createBootcamp)
 
 // router
