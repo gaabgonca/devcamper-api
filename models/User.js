7590726196659b023f98fe77c3b53889dd27ebec
[Gabriel GC] <[gaabgonca@gmail.com]>
2021-04-12 08:59:02 -0500

    Created hashed password reset and stored it in user model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
@@ -49,6 +50,23 @@ UserSchema.methods.getSignedJwtToken = function () {
 // Match user entered password to hashed password in database
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
+}
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+  //Generate Token
+  const resetToken = crypto.randomBytes(20).toString('hex')
+
+  //Hash Token
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex')
+
+  //Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000
+
+  return resetToken
 }
 
 module.exports = mongoose.model('User', UserSchema)
